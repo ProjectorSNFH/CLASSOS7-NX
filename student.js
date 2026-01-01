06361551146fdbd4d17d5a0e8ad3c4a2269aeddf
TheLostIslandX <jaewonprojects@gmail.com>
2026-01-02 01:04:11 +0900

    Update student.js

--- student.js
+++ student.js
@@ -2,14 +2,14 @@
 const students = [
   {
     number: 1,
-    name: "홍길동",
+    name: "권윤서",
     id: "std01",
     password: "pw01!",
     role: "N"
   },
   {
     number: 2,
-    name: "이순신",
+    name: "김이은",
     id: "std02",
     password: "pw02!",
     role: "T"
