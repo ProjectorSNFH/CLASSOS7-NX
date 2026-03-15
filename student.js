a5a7d903b527ef64b1f9383648e9fcd26cccac63
ProjectorSNFH <whatwhat0108@icloud.com>
2026-03-15 09:24:37 +0900

    Std Set

--- student.js
+++ student.js
@@ -1,19 +1,180 @@
 // student.js
 const students = [
   {
-    number: 0,
-    name: "게스트",
-    id: "guest",
-    password: "password",
+    number: 1,
+    name: "권윤서",
+    id: "sym01",
+    password: "pw01",
     role: "N"
   },
   {
-    number: 0,
-    name: "관리자",
-    id: "administrator",
-    password: "password",
+    number: 2,
+    name: "권지윤",
+    id: "sym02",
+    password: "pw02",
+    role: "N"
+  },
+   {
+    number: 3,
+    name: "김민하",
+    id: "sym03",
+    password: "pw03",
+    role: "N"
+  },
+  {
+    number: 4,
+    name: "김원우",
+    id: "sym04",
+    password: "pw04",
+    role: "N"
+  },
+  {
+    number: 5,
+    name: "김주원",
+    id: "sym05",
+    password: "pw05",
+    role: "N"
+  },
+   {
+    number: 6,
+    name: "김현수",
+    id: "sym06",
+    password: "pw06",
+    role: "N"
+  },
+    {
+    number: 7,
+    name: "노은채",
+    id: "sym07",
+    password: "pw07",
+    role: "N"
+  },
+  {
+    number: 8,
+    name: "류예준",
+    id: "sym08",
+    password: "pw08",
+    role: "N"
+  },
+   {
+    number: 9,
+    name: "박해림",
+    id: "sym09",
+    password: "pw09",
+    role: "N"
+  },
+  {
+    number: 10,
+    name: "배수진",
+    id: "sym10",
+    password: "pw10",
+    role: "N"
+  },
+  {
+    number: 11,
+    name: "서동아",
+    id: "sym11",
+    password: "pw11",
+    role: "N"
+  },
+   {
+    number: 12,
+    name: "서용현",
+    id: "sym12",
+    password: "pw12",
+    role: "N"
+  },
+  {
+    number: 13,
+    name: "안소율",
+    id: "sym13",
+    password: "pw13",
+    role: "N"
+  },
+  {
+    number: 14,
+    name: "이규원",
+    id: "sym14",
+    password: "pw14",
+    role: "N"
+  },
+   {
+    number: 15,
+    name: "이승은",
+    id: "sym15",
+    password: "pw15",
+    role: "N"
+  },
+  {
+    number: 16,
+    name: "이재원",
+    id: "admin",
+    password: "password!",
     role: "A"
   },
+  {
+    number: 17,
+    name: "정윤하",
+    id: "sym17",
+    password: "pw17",
+    role: "N"
+  },
+   {
+    number: 18,
+    name: "최다연",
+    id: "sym18",
+    password: "pw18",
+    role: "N"
+  },
+    {
+    number: 19,
+    name: "하유민",
+    id: "sym19",
+    password: "pw19",
+    role: "N"
+  },
+  {
+    number: 20,
+    name: "홍우빈",
+    id: "administrator",
+    password: "password!",
+    role: "A"
+  },
+   {
+    number: 21,
+    name: "하윤",
+    id: "sym21",
+    password: "pw21",
+    role: "N"
+  },
+   {
+    number: 22,
+    name: "홍유상",
+    id: "sym22",
+    password: "pw22",
+    role: "N"
+  },
+  {
+    number: 23,
+    name: "황샘",
+    id: "sym23",
+    password: "pw23",
+    role: "N"
+  },
+  {
+    number: 24,
+    name: "황재영",
+    id: "sym24",
+    password: "pw24",
+    role: "N"
+  },
+   {
+    number: 0,
+    name: "게스트 방문자",
+    id: "guest",
+    password: "password",
+    role: "N"
+  }
 ];
 
 module.exports = students;
